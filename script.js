7d7955506639fc1ce6e366b241bf2b445aa55e28
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-09-20 07:50:03 +0200

    add 2 const, 1 for querySelector ul and another one for createElement li

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const button = document.querySelector("enter");
 const button = document.querySelector("usrinput");
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
