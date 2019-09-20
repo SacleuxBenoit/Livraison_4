4bb4d3612bfae920d70fc909ed9f0f9a1d190e25
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-09-20 09:10:53 +0200

    add 2 functions addTextNode and resetInput in script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const button = document.getElementById("enter");
 const button = document.getElementById("usrinput");
 const ul = document.querySelector("ul");
 const li = document.createElement("li");
+
+function inputlength(){
+    return input.value.length;
+}
+
+function addTextNode(){
+    li.insertAdjacentText("beforeend", document.createTextNode(input,value));
+}
+
+function resetInput(){
+    input.value ="";
+}
